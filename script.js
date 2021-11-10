0c0ee18396b96a2ae0e09a365feb8c6561f5bd45
Scott Thompson <sthompson082290@gmail.com>
2021-11-09 19:23:28 -0600

    Color Flipper Project 11/9/21

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function changeColor() {
-     var newColor = makeColor();
-     var box = document.getElementById('box').style.backgroundColor = newColor;
+     const newColor = makeColor();
+     document.getElementById('box').style.backgroundColor = newColor;
      document.getElementById('rgb').innerHTML = newColor;
 }
+function printNumbers() {
+     for (const n of myArr) {
+      document.getElementById('box').innerHTML += `<div>${n}</div>`;
+    
+    }
+    }
+
 
 function makeColor() {
-     var arr = [];
-     for(var i = 0; i < 3; i ++){
-          var num = Math.floor(Math.random() * 256)
-          arr.push(num);
-     }
-     var newRgb = 'rgb(' + arr[0] + ',' + arr[1] + ',' + arr[2] + ')';
+     const arr = [];
+     for(let i = 0; i < 3; i ++){
+          const num = Math.floor(Math.random() * 256) //creates a random number between 0 - 255 anywhere from 0000 0000 to all 1111 1111 (256 options)
+          arr.push(num); //math.floor rounds down the decimal to the normal number math.ceil rounds up
+     } 
+     return 'rgb(' + arr[0] + ',' + arr[1] + ',' + arr[2] + ')';
+      //math.random will give you a random number anywhere between 0 - .999999~
+     const newRgb = 'rgb(' + arr[0] + ',' + arr[1] + ',' + arr[2] + ')';
      return newRgb;
 }
